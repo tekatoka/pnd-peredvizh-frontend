5901f911314c6e3a2dec906c36a7994b2336a119
Bogdana Bondarenko <bogdana.bondarenko@jaemacom.de>
2021-11-19 00:58:01 +0100

    added axios configuration

--- src/axios.config.js
+++ src/axios.config.js
@@ -1,11 +1,20 @@
 import axios from 'axios';
 import config from './config';
 
+// peredvizhUser
+
+// post('http://{api base url}/auth/local', {
+//   identifier: 'online@panda-platforma.berlin',
+//   password: 'P!GpkjB%xkNG2x?wrbTf7QwfegWf',
+// });
+
+const jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjM3Mjc5NTcxLCJleHAiOjE2Mzk4NzE1NzF9.9TXKimPP5ose6sUN0lYGLcfbLVLoww5JCE3xGTuA2fc";
+
 const instance = axios.create({
     baseURL: config.api.baseUrl,
     headers: {
       common: {
-        //Authorization: 'AUTH_TOKEN_FROM_INSTANCE'
+        Authorization: `Bearer ${jwt}`
       }
     }
   });
