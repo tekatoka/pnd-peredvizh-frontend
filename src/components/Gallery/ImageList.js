c2ec987103b7fbc646f2d7357bd9dc7d6bd334ed
Bogdana Bondarenko <bogdana.bondarenko@jaemacom.de>
2021-11-24 15:58:54 +0100

    fixed image thumbnail

--- src/components/Gallery/ImageList.js
+++ src/components/Gallery/ImageList.js
@@ -39,6 +39,7 @@ const ImageList = (props) => {
     <React.Fragment>
       <ModalDialog>
           <ImageCarousel items={items} activeItem={currentItemId} />
+        {/* {items && items.length} - id {currentItemId} */}
       </ModalDialog>
       <Row>
         {items &&
@@ -46,7 +47,7 @@ const ImageList = (props) => {
             const imageUrl = item.Image
               ? item.ImageUrl
                 ? item.ImageUrl
-                : item.Image.formats["small"].url
+                : item.Image.formats["small"] ? item.Image.formats["small"].url : item.Image.formats["thumbnail"].url
               : NoImage;
             const description = type == "people" ? item.Name : "";
             const link = type == "people" ? "/people/" + item.slug : "";
